0cd43e6e7316bacee5958fd67397e14eb820e00a
Eliott Rasumny <eliott.rasumny@student.vinci.be>
2022-09-23 10:47:14 +0200

    module2 : init

---
+++ exercises/module2/src/components/App/App.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+import Button from "components/Button/Button";
+import Display from "components/Display/Display";
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  const increaseByOne = () => setCounter(counter + 1);
+  const decreaseByOne = () => setCounter(counter - 1);
+  const setToZero = () => setCounter(0);
+
+  return (
+    <div>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={setToZero} text="zero" />
+      <Button onClick={decreaseByOne} text="minus" />
+    </div>
+  );
+};
+
+export default App;
