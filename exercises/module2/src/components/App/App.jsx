50295120a9a2a9ea43f362bed8ad9afde26d823c
Eliott Rasumny <eliott.rasumny@student.vinci.be>
2022-09-30 10:27:46 +0200

    module2 : localStorage

---
+++ exercises/module2/src/components/App/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+import Button from "components/Button/Button";
+import Display from "components/Display/Display";
+
+const App = () => {
+  const [counter, setCounter] = useState(
+    JSON.parse(localStorage.getItem("counter"))
+  );
+
+  const changeCount = (delta) => {
+    localStorage.setItem("counter", JSON.stringify(counter + delta));
+    setCounter(counter + delta);
+  };
+
+  return (
+    <div>
+      <Display counter={counter} />
+      <Button changeCount={changeCount} text="plus" delta={1} />
+      <Button changeCount={changeCount} text="zero" delta={-counter} />
+      <Button changeCount={changeCount} text="minus" delta={-1} />
+    </div>
+  );
+};
+
+export default App;
