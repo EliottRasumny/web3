f92ac3c5342f37a9b02d632f46c2ba186d95426f
Eliott Rasumny <eliott.rasumny@student.vinci.be>
2022-10-04 15:04:29 +0200

    module2 : custom hook

--- exercises/module2/src/components/App/App.jsx
+++ exercises/module2/src/components/App/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from "react";
 
 import Button from "components/Button/Button";
 import Display from "components/Display/Display";
+import useLocalStorage from "hooks/useLocalStorage";
 
 const App = () => {
-  const [counter, setCounter] = useState(
-    JSON.parse(localStorage.getItem("counter"))
-  );
+
+  const [counter, setCounter] = useLocalStorage("counter", 0);
 
   const changeCount = (delta) => {
-    localStorage.setItem("counter", JSON.stringify(counter + delta));
     setCounter(counter + delta);
   };
 
--- exercises/module2/src/hooks/useLocalStorage.js
+++ exercises/module2/src/hooks/useLocalStorage.js
@@ -1,9 +1,19 @@
-const useLocalStorage = (key) => {
-  if (localStorage.getItem(key) == undefined) {
-    //create
-  }
+import { useState } from "react";
 
-  //TODO
+const useLocalStorage = (key, initialValue) => {
+  if (localStorage.getItem(key) === null) {
+    localStorage.setItem(key, JSON.stringify(initialValue));
+  }
+  const [valueState, setterState] = useState(
+    JSON.parse(localStorage.getItem(key))
+  );
+
+  const setter = (newValue) => {
+    localStorage.setItem(key, JSON.stringify(newValue));
+    setterState(newValue);
+  };
+
+  return [valueState, setter];
 };
 
 export default useLocalStorage;
